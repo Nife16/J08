89aad87addd40820b9d7d721ebda97838f630ede
Nife16 <100443715+Nife16@users.noreply.github.com>
2022-12-09 13:10:09 -0600

    added in class spring mvc project, has notes check them out!

--- weekFive/demoAgain!/src/main/java/com/example/controller/UserController.java
+++ weekFive/demoAgain!/src/main/java/com/example/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.entity.User;
 import com.example.service.UserService;
